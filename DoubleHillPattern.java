7689afce7f67a9a5a3e4991f28edbb8e4dda3b66
Krishna rathor <rathorkrishna142@gmail.com>
2026-07-05 20:53:19 +0530

    Add DoubleHillPattern class to print double hill pattern

---
+++ DoubleHillPattern.java
@@ -0,0 +1,28 @@
+public class DoubleHillPattern {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 1; j <= (2 * (n - i)); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
