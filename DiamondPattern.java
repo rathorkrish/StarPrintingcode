e8c1bced1a9e4a1654566f01dc3b328e3d898eaf
Krishna rathor <rathorkrishna142@gmail.com>
2026-07-05 20:54:04 +0530

    Add DiamondPattern class to print diamond shape

---
+++ DiamondPattern.java
@@ -0,0 +1,36 @@
+public class DiamondPattern {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        // Upper
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
